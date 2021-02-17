a0ccc13a925744b653718c499914717adb27e5ea
codermanez <75057913+codermanez@users.noreply.github.com>
2021-02-17 14:54:09 -0500

    Update mainsrc.js

--- mainsrc.js
+++ mainsrc.js
@@ -4,7 +4,10 @@ var gameData = {
 }
 var genData = {
   T1amnt: 0,
-  T1cost: 100
+  T1cost: 100,
+  T2amnt: 0,
+  T2cost: 10000,
+  T2mult: 20
 }
 function mineAntimass() {
   gameData.antimass += gameData.antimassPerClick
@@ -22,8 +25,23 @@ function addGenT1() {
     setTimeout(document.getElementById("genT1btn").innerHTML = "Build Anti-Mass Generator Tier 1", 1000)
   }
 }
+function addGenT2() {
+  if (gameData.antimass >= genData.T2cost) {
+    gameData.antimass -= genData.T2cost
+    genData.T2amnt += 1
+    if (genData.T2amnt % 10 == 0) {
+      genData.T2cost *= genData.T2cost * 2.5
+    }
+  }
+  else {
+    document.getElementById("genT2btn").innerHTML = "Sorry, not enough Anti-Mass"
+    setTimeout(document.getElementById("genT2btn").innerHTML = "Build Anti-Mass Generator Tier 2", 1000)
+  }
+}
 var intervalId = window.setInterval(function(){
   document.getElementById("AntimassGenerated").innerHTML = gameData.antimass + " Anti-Mass Generated"
   document.getElementById("amntGenT1").innerHTML = "Cost: " + genData.T1cost + ", You Have " + genData.T1amnt + " Tier 1 Anti-Mass Generators (AMGs)"
+  document.getElementById("amntGenT2").innerHTML = "Cost: " + genData.T2cost + ", You Have " + genData.T2amnt + " Tier 2 Anti-Mass Generators (AMGs)"
   gameData.antimass += genData.T1amnt
+  gameData.antimass += genData.T2amnt * genData.T2mult
 }, 10);
