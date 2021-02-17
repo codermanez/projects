97401e6b96921a05f0aca2ef95b2b65d431601f0
codermanez <75057913+codermanez@users.noreply.github.com>
2021-02-17 13:26:56 -0500

    Create mainsrc.js

---
+++ mainsrc.js
@@ -0,0 +1,11 @@
+var gameData = {
+  antimass: 0,
+  antimassPerClick: 1
+}
+function mineAntimass() {
+  gameData.antimass += gameData.antimassPerClick
+}
+function updateScript() {
+  document.getElementById("goldMined").innerHTML = gameData.antimass + " Anti-Mass Mined"
+}
+var intervalId = window.setInterval(updateScript(), 10);
