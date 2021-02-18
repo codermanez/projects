6de8224511d63b379bf0bb291cfc0b4395405ff4
codermanez <75057913+codermanez@users.noreply.github.com>
2021-02-18 15:54:25 -0500

    Update mainincrementor.js

--- polynomialexpansion/mainincrementor.js
+++ polynomialexpansion/mainincrementor.js
@@ -6,5 +6,5 @@ function createJoule() {
   OmegaNum.add(joules, jpc)
 }
 var mainUpdate = window.setInterval(function(){ // runs every 10ms, 100 times/sec
-  document.getElementById("AntimassGenerated").innerHTML = clickData.joules + "J Generated"
+  document.getElementById("jouleCount").innerHTML = clickData.joules + "J Generated"
 }, 10)
