71a6e0f94224b4788902d89d7dfb02eab0d4f146
codermanez <75057913+codermanez@users.noreply.github.com>
2021-02-18 23:49:49 -0500

    Update mainincrementor.js

--- polynomialexpansion/mainincrementor.js
+++ polynomialexpansion/mainincrementor.js
@@ -7,7 +7,10 @@ var clickData = {
   JGenT1Mult: new Decimal("1"),
   JGenT2: new Decimal("0"),
   JGenT2Cost: new Decimal("2000"),
-  JGenT2Mult: new Decimal("1")
+  JGenT2Mult: new Decimal("1"),
+  JGenT3: new Decimal("0"),
+  JGenT3Cost: new Decimal("500000"),
+  JGenT3Mult: new Decimal("1")
 }
 function createJoule() {
   clickData.joules = clickData.joules.add(clickData.jpc)
@@ -26,12 +29,21 @@ function addgenT2() {
     clickData.JGenT2Cost = clickData.JGenT2Cost.pow(1.05).mul(clickData.costMultiplier).ceil()
   }
 }
+function addgenT3() {
+  if (clickData.joules.gte(clickData.JGenT3Cost)) {
+    clickData.joules = clickData.joules.sub(clickData.JGenT3Cost)
+    clickData.JGenT3 = clickData.JGenT3.add(1)
+    clickData.JGenT3Cost = clickData.JGenT3Cost.pow(1.05).mul(clickData.costMultiplier).ceil()
+  }
+}
 var mainUpdate = window.setInterval(function(){ // runs every 10ms, 100 times/sec
   document.getElementById("jouleCount").innerHTML = clickData.joules + "J Generated"
   document.getElementById("JGenT1Count").innerHTML = "Cost: " + clickData.JGenT1Cost + ", You Have " + clickData.JGenT1 + " Tier 1 Energy Generators"
   document.getElementById("JGenT2Count").innerHTML = "Cost: " + clickData.JGenT2Cost + ", You Have " + clickData.JGenT2 + " Tier 2 Energy Generators"
+  document.getElementById("JGenT3Count").innerHTML = "Cost: " + clickData.JGenT3Cost + ", You Have " + clickData.JGenT3 + " Tier 3 Energy Generators"
   clickData.joules = clickData.joules.add(clickData.JGenT1.mul(clickData.JGenT1Mult))
 }, 10)
 var genUpdate = window.setInterval(function(){
   clickData.JGenT1 = clickData.JGenT1.add(clickData.JGenT2.mul(clickData.JGenT2Mult))
+  clickData.JGenT2 = clickData.JGenT2.add(clickData.JGenT3.mul(clickData.JGenT3Mult))
 }, 1000)
