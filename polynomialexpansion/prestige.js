b090b824c9842a15faa9d8d2c2af6d749de6d71a
codermanez <75057913+codermanez@users.noreply.github.com>
2021-02-20 02:17:51 -0500

    Update prestige.js

--- polynomialexpansion/prestige.js
+++ polynomialexpansion/prestige.js
@@ -1,1 +1,42 @@
-
+var prestige = {
+  alpha: new Decimal("0"),
+  beta: new Decimal("0"),
+  gamma: new Decimal("0"),
+  delta: new Decimal("0")
+}
+function prestigeAlpha() {
+  if(clickData.JGenT16.gte(5)){
+    clickData.joules = new Decimal("0")
+    clickData.JGenT1 = new Decimal("0")
+    clickData.JGenT2 = new Decimal("0")
+    clickData.JGenT3 = new Decimal("0")
+    clickData.JGenT4 = new Decimal("0")
+    clickData.JGenT5 = new Decimal("0")
+    clickData.JGenT6 = new Decimal("0")
+    clickData.JGenT7 = new Decimal("0")
+    clickData.JGenT8 = new Decimal("0")
+    clickData.JGenT9 = new Decimal("0")
+    clickData.JGenT10 = new Decimal("0")
+    clickData.JGenT12 = new Decimal("0")
+    clickData.JGenT13 = new Decimal("0")
+    clickData.JGenT14 = new Decimal("0")
+    clickData.JGenT15 = new Decimal("0")
+    clickData.JGenT16 = new Decimal("0")
+    clickData.JGenT1Mult = new Decimal("0")
+    clickData.JGenT2Mult = new Decimal("0")
+    clickData.JGenT3Mult = new Decimal("0")
+    clickData.JGenT4Mult = new Decimal("0")
+    clickData.JGenT5Mult = new Decimal("0")
+    clickData.JGenT6Mult = new Decimal("0")
+    clickData.JGenT7Mult = new Decimal("0")
+    clickData.JGenT8Mult = new Decimal("0")
+    clickData.JGenT9Mult = new Decimal("0")
+    clickData.JGenT10Mult = new Decimal("0")
+    clickData.JGenT12Mult = new Decimal("0")
+    clickData.JGenT13Mult = new Decimal("0")
+    clickData.JGenT14Mult = new Decimal("0")
+    clickData.JGenT15Mult = new Decimal("0")
+    clickData.JGenT16Mult = new Decimal("0")
+    prestige.alpha = prestige.alpha.add(1)
+  }
+}
