6c6eeaafbc4f00616dcc47c6eadac412930cd362
codermanez <75057913+codermanez@users.noreply.github.com>
2021-02-20 14:43:54 -0500

    Update prestige.js

--- polynomialexpansion/prestige.js
+++ polynomialexpansion/prestige.js
@@ -2,7 +2,27 @@ var prestige = {
   alpha: new Decimal("0"),
   beta: new Decimal("0"),
   gamma: new Decimal("0"),
-  delta: new Decimal("0")
+  delta: new Decimal("0"),
+  epsilon: new Decimal("0"),
+  zeta: new Decimal("0"),
+  eta: new Decimal("0"),
+  theta: new Decimal("0"),
+  iota: new Decimal("0"),
+  kappa: new Decimal("0"),
+  lambda: new Decimal("0"),
+  mu: new Decimal("0"),
+  nu: new Decimal("0"),
+  xi: new Decimal("0"),
+  omicron: new Decimal("0"),
+  pi: new Decimal("0"),
+  rho: new Decimal("0"),
+  sigma: new Decimal("0"),
+  tau: new Decimal("0"),
+  upsilon: new Decimal("0"),
+  phi: new Decimal("0"),
+  chi: new Decimal("0"),
+  psi: new Decimal("0"),
+  omega: new Decimal("0")
 }
 function prestigeAlpha() {
   if(clickData.JGenT16.gte(5)){
