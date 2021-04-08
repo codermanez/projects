2876586d88c0dd24a5941e15a81c8c80b17f848c
codermanez <75057913+codermanez@users.noreply.github.com>
2021-04-08 15:12:19 -0400

    something i made at school

---
+++ textuwuifier/uwu.js
@@ -0,0 +1,43 @@
+const Uwuifier = require('uwuifier')
+const fs = require('fs')
+const fetch = require('node-fetch')
+var uwuedtxt;
+const uwuifier = new Uwuifier({
+	spaces: {
+        faces: 0.03,
+        actions: 0.015,
+        stutters: 0.1
+    },
+    words: 1,
+    exclamations: 1
+});
+var option = 'web';
+if (option == 'web') {
+	console.log('Getting file to UwU...')
+	fetch('http://textfiles.com/science/coldfusn.txt')
+		.then(res => res.text())
+		.then(body => {
+			console.log('Got File!, Attempting to UwU now...')
+			uwuedtxt = uwuifier.uwuifySentence(body)
+			fs.writeFile('uwuedfile.txt', uwuedtxt, err => {
+				if (err) {
+					console.error(err)
+				}
+			})
+			console.log('File has been UwUed!')
+		});
+} else if (option == 'file') {
+	fs.readFile('filetouwu.txt', 'utf8', (err, data) => {
+		if (err) {
+			console.error(err)
+		}
+		console.log('UwUing file...')
+		var content = uwuifier.uwuifySentence(data);
+		fs.writeFile('uwuedfile.txt', content, err => {
+			if (err) {
+				console.error(err)
+			}
+		});
+		console.log('Finished UwUing file!')
+	});
+}
